89f067a38d2fc8b5323c857977fccb2ed92f2a8a
AnthonyRamirezNC <thearamirez2020@gmail.com>
2024-09-18 16:47:27 -0400

    added main file to test account classes, added tests in main class for savings

--- BankAccount.py
+++ BankAccount.py
@@ -29,30 +29,3 @@ class BankAccount:
         print("Customer: " + self.customer_name)
         print("Current Balance: " + str(self.current_balance))
         print("Minimum Balance: " + str(self.minimum_balance) + "\n")
-
-
-
-
-
-
-
-#test out code with 2 instances
-customerAccount1 = BankAccount("Anthony Ramirez", 25, 5)
-#test deposit
-customerAccount1.deposit(10)
-#test withdraw
-customerAccount1.withdraw(5)
-#test withdraw over limit
-customerAccount1.withdraw(45)
-#test print customer info
-customerAccount1.print_customer_information()
-
-customerAccount2 = BankAccount("Robert Ramirez", 15, 10)
-#test deposit
-customerAccount2.deposit(15)
-#test withdraw
-customerAccount2.withdraw(10)
-#test withdraw over limit
-customerAccount2.withdraw(32)
-#test print customer info
-customerAccount2.print_customer_information()
