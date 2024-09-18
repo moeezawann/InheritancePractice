c97008e6b5c7ca5d66256c9b5c8f169a8b87e17b
AnthonyRamirezNC <thearamirez2020@gmail.com>
2024-09-18 16:27:45 -0400

    Implemented interest method

--- savings.py
+++ savings.py
@@ -1,7 +1,15 @@
-import BankAccount
+from BankAccount import *
+
 class savings(BankAccount):
-    def __init__():
-        super().__init__()
+    def __init__(self, customer_name, current_balance, minimum_balance, routingNum, accountNum):
+        super().__init__(customer_name, current_balance, minimum_balance)
+        self.__routingNum = routingNum
+        self.__accountNum = accountNum
+
+    def addInterest(self):
+        #add 5 percent interest
+        self.deposit(self.getBalance() * .05)
+
 
     def printTest():
         print("test")
